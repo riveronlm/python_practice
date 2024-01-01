3b7fe78d1ed9857da63885a5ff858ae7d0a86ef5
riveronlm <riveron.lm@gmail.com>
2024-01-01 15:56:33 -0500

    dictionary practice

--- ceasar_cypher.py
+++ ceasar_cypher.py
@@ -8,12 +8,16 @@ def caeser(start_text,shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
        shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
+  for char in start_text:
+    position = alphabet.index(char)
     new_position = position +shift_amount
     end_text += alphabet[new_position]
+  else:
+    end_text += char  
   print(f"The {cipher_direction}d text is {end_text}")     
   
+from logo import logo
+print(logo)
 
-   
+shift = shift % 26
 caeser(start_text=text, shift_amount=shift, cipher_direction=direction)         
